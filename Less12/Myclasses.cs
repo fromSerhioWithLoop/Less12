8263130523fe95fbafbe92333cd6a46267f94e92
SerhioGacura <sgatfor@gmail.com>
2020-01-30 17:47:54 +0300

    Добавьте файлы проекта.

---
+++ Less12/Myclasses.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less12
+{
+    class Figure
+    {
+        public string Name { get; set; } = "noName";
+        public int Area { get; set; }
+        public object FigType { get; set; }
+
+
+        public virtual int FigureArea()
+        {
+            return 0;
+        }
+
+
+    }
+    class Square : Figure
+    {
+        public int SideA { get; set; } = 0;
+
+        public override int FigureArea()
+        {
+            return SideA * SideA;
+        }
+    }
+    class Rectangle : Figure
+    {
+        public int SideA { get; set; } = 0;
+        public int SideB { get; set; } = 0;
+
+        public override int FigureArea()
+        {
+
+            return Area = SideA * SideB;
+        }
+    }
+
+    class Triangle : Figure
+    {
+        public int BaseA { get; set; } = 0;
+        public int Height { get; set; } = 0;
+
+        public override int FigureArea()
+        {
+            return Area = BaseA * Height / 2;
+        }
+    }
+
+    class Circle : Figure
+    {
+        const double Pi = 3.1416;
+        public int Radius { get; set; } = 0;
+
+        public override int FigureArea()
+        {
+            return Area = (int)(Pi * Radius * Radius);
+
+        }
+
+    }
+
+    public class Engine
+    {
+        public Object[] GetMass(int NumberOfFigures)
+        {
+            Figure[] figure = new Figure[NumberOfFigures];
+            int count = 0;
+            int figuresTypesCount=3;
+            Random rand = new Random();
+
+            while (count < NumberOfFigures)
+            {
+
+                if (count % figuresTypesCount == 0)
+                {
+                    Square square = new Square() { SideA = rand.Next(10) };
+                    Figure fig = new Figure
+                    {
+                        Name = "Square",
+                        Area = square.FigureArea(),
+                        FigType = square.GetType()
+                    };
+                    figure[count] = fig;
+                }
+                else if (count % figuresTypesCount == 2)
+                {
+                    Triangle triangle = new Triangle() { BaseA = rand.Next(10), Height = rand.Next(10) };
+                    Figure fig = new Figure
+                    {
+                        Name = "Triangle",
+                        Area = triangle.FigureArea(),
+                        FigType = triangle.GetType()
+                    };
+                    figure[count] = fig;
+                }
+                else if (count % figuresTypesCount == 1)
+                {
+                    Circle circle = new Circle() { Radius = rand.Next(10) };
+                    Figure fig = new Figure
+                    {
+                        Name = "Circle",
+                        Area = circle.FigureArea(),
+                        FigType = circle.GetType()
+                    };
+                    figure[count] = fig;
+                }
+                count++;
+            }
+            return figure;
+        }
+
+
+    }
+}
---
+++ Less12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberFigures = 5; // требуемое колво фигур(если 5 - будет 2 квадрата 2 круга и 1 треугольник)
+            Engine e = new Engine();
+            Object[] arr = e.GetMass(NumberFigures);
+            int i;
+            for (i = 0; i < NumberFigures; i++)
+            {
+                Figure figure = new Figure();
+                figure = (Figure)arr[i];
+                Console.WriteLine($"\n   {i + 1}\t This is {figure.Name}. CLR type is {figure.FigType}. Square is {figure.Area}");
+            }
+            Console.WriteLine("\n\t Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
